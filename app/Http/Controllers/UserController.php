7cadb0625c6008872502934bf60e6a347d0bf139
vagrant <vagrant@homestead>
2018-06-28 06:36:50 +0000

    authorized user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,19 +5,40 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Requests\UserRequest;
 use App\Models\User;
+use App\Handlers\ImageUploadHandler;
 class UserController extends Controller
 {
-   public function show(User $user)
+	public function __construct()
+	{
+		$this->middleware('auth', ['except' => ['show']]);
+	}
+    public function show(User $user)
     {
         return view('users.show', compact('user'));
     }
     public function edit(User $user)
     {
+    	$this->authorize('update', $user);
         return view('users.edit', compact('user'));
     }
-    public function update(UserRequest $request, User $user)
+    public function update(UserRequest $request, ImageUploadHandler $uploader, User $user)
     {
-        $user->update($request->all());
+    	$this->authorize('update', $user);
+    	$data = $request->all();
+    	if ($request->avatar) {
+    		$result = $uploader->save($request->avatar, 'avatars', $user->id, 362);
+    		if ($result) {
+    			//删除原图
+    			if ($user->avatar) {
+	    			$res = str_replace(config('app.url'), public_path(), $user->avatar);
+	    			if (file_exists($res)) {
+	    			 	unlink($res);
+	    			}
+    			}
+    			$data['avatar'] = $result['path'];
+    		}
+    	}
+        $user->update($data);
         return redirect()->route('users.show', $user->id)->with('success', '个人资料更新成功！');
     }
 
